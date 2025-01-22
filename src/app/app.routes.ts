5aac1efbe0f4fef7e84b4ced56b2fd803ada8588
AminMahmoudAhmed <aminmahmoudahmed2000@gmail.com>
2025-01-22 16:40:33 +0200

    initial commit

---
+++ src/app/app.routes.ts
@@ -0,0 +1,16 @@
+import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { ContactComponent } from './contact/contact.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+
+export const routes: Routes = [
+    {path: '', redirectTo: 'home', pathMatch: 'full'},
+   {path:'home',component:HomeComponent},
+   {path:'about',component:AboutComponent},
+   {path:'protfilio',component:PortfolioComponent},
+   {path:'contact',component:ContactComponent},
+   {path:'**',component:NotFoundComponent}
+   
+];
